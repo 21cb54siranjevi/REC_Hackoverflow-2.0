de055de5f33775cdb55326d101030b4280104b3a
21cb54siranjevi <siranjevi.k2021@kgkite.ac.in>
2022-04-02 14:37:02 +0530

    update on flutter

---
+++ bigbro/lib/locator.dart
@@ -0,0 +1,12 @@
+import 'camera.service.dart';
+import 'ml_service.dart';
+import 'face_detector_service.dart';
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.instance;
+
+void setupServices() {
+  locator.registerLazySingleton<CameraService>(() => CameraService());
+  locator.registerLazySingleton<FaceDetectorService>(() => FaceDetectorService());
+  locator.registerLazySingleton<MLService>(() => MLService());
+}
